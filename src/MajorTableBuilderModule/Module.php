62afa8ecdca6c3c3d221fafa76484868e305780e
Rob Caiger <rob@clocal.co.uk>
2014-07-13 12:56:30 +0100

    Autoloader config

--- src/MajorTableBuilderModule/Module.php
+++ src/MajorTableBuilderModule/Module.php
@@ -15,9 +15,20 @@ use Zend\Mvc\MvcEvent;
 
 class Module implements AutoloaderProviderInterface
 {
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__)
+                )
+            )
+        );
+    }
+
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '../../config/module.config.php';
     }
 
     public function onBootstrap(MvcEvent $e)
